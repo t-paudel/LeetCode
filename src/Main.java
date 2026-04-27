b864b77afd837ee1147a826fca8d6916d7f145d3
tpaudel <tushar.paudel884@gmail.com>
2026-04-27 09:56:46 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import arraysAndList.easy.RemoveDuplicates;
+
+void main() {
+    int[] nums = new int[]{1,1,3,4,5,6,6,7};
+    int size = RemoveDuplicates.removeDuplicates(nums);
+    System.out.println(size);
+}
