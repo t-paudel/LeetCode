34d74e42fbf76970ffaec0fdaf845bbc8851b038
tushar <tushar.paudel884@gmail.com>
2026-07-16 16:08:00 +0530

    AddTwoNumbers

---
+++ src/code/companies/medium/AddTwoNumbers.java
@@ -0,0 +1,88 @@
+/*
+    You are given two non-empty linked lists representing two non-negative integers. The digits are
+    stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and
+    return the sum as a linked list.
+
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+    Input: l1 = [2,4,3], l2 = [5,6,4]
+    Output: [7,0,8]
+    Explanation: 342 + 465 = 807.
+    Example 2:
+
+    Input: l1 = [0], l2 = [0]
+    Output: [0]
+    Example 3:
+
+    Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+    Output: [8,9,9,9,0,0,0,1]
+
+    Constraints:
+
+    The number of nodes in each linked list is in the range [1, 100].
+    0 <= Node.val <= 9
+    It is guaranteed that the list represents a number that does not have leading zeros.
+ */
+
+package code.companies.medium;
+
+public class AddTwoNumbers {
+    static void main() {
+//        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+//        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        ListNode l1 = new ListNode(9);
+        ListNode l2 = new ListNode(1,
+                new ListNode(9,
+                new ListNode(9,
+                new ListNode(9,
+                new ListNode(9,
+                new ListNode(9,
+                new ListNode(9,
+                new ListNode(9,
+                new ListNode(9,
+                new ListNode(9))))))))));
+
+        ListNode result = addTwoNumbers(l1, l2);
+        while(result != null) {
+            System.out.println(result.val);
+            result = result.next;
+        }
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode();
+        ListNode curr = head;
+
+        int carry = 0;
+
+        while(l1 != null || l2 != null || carry != 0) {
+            int sum = carry;
+
+            if(l1 != null) {
+                sum += l1.val;
+                l1 = l1.next;
+            }
+
+            if(l2 != null) {
+                sum += l2.val;
+                l2 = l2.next;
+            }
+
+            carry = sum / 10;
+
+            curr.next = new ListNode(sum%10);
+            curr = curr.next;
+        }
+
+        return head.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
